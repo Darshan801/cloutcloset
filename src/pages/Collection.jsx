00ce200f465879d854bc7b391ab0d119104e7140
Darshan801 <darshankunwar79@gmail.com>
2025-03-15 16:18:12 +0545

    latest footer

--- src/pages/Collection.jsx
+++ src/pages/Collection.jsx
@@ -9,17 +9,11 @@ const Collection = () => {
   const [showfilter,setshowfilter]=useState(false);
   const[FilterProducts,setFilterProducts]=useState([]);
   const [category,setCategory]=useState([]);
-<<<<<<< HEAD
   const [subcategory,setSubCategory]=useState([]);
 
 
   //for categary
   const toggleCategory=(e)=>{
-=======
-  const [subcategary,setSubCategory]=useState([]);
-
-  const toggleCategary=(e)=>{
->>>>>>> 555b1b9c59e4828476de91d078265de6d9a94efc
     if (category.includes(e.target.value)) {
       setCategory(prev=>prev.filter(item=>item !== e.target.value))
     }
@@ -28,22 +22,16 @@ const Collection = () => {
     }
   }
 
-<<<<<<< HEAD
   //for type or subcatogary
 
   const toggleSubCategory=(e)=>{
     if (subcategory.includes(e.target.value)) {
-=======
-  const toggleSubCategary=(e)=>{
-    if (subcategary.includes(e.target.value)) {
->>>>>>> 555b1b9c59e4828476de91d078265de6d9a94efc
       setSubCategory(prev=>prev.filter(item=>item !== e.target.value))
     }
     else{
       setSubCategory(prev => [...prev,e.target.value])
     }
   }
-<<<<<<< HEAD
   
   const applyFilter=()=>{
     let productCopy=products.slice();
@@ -59,24 +47,15 @@ const Collection = () => {
   // useEffect(()=>{
   //     console.log(category);
   // },[category])
-=======
-
-  useEffect(()=>{
-      console.log(subcategary);
-  },[subcategary])
->>>>>>> 555b1b9c59e4828476de91d078265de6d9a94efc
 
   useEffect(()=>{
     setFilterProducts(products);
   },[])
-<<<<<<< HEAD
 
   useEffect(()=>{
     applyFilter();
   },[category,subcategory])
 
-=======
->>>>>>> 555b1b9c59e4828476de91d078265de6d9a94efc
   return (
     <div className='flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t'>
 
@@ -90,7 +69,6 @@ const Collection = () => {
             <p className='mb-3 text-sm font-medium'>CATEGORIES</p>
               <div className='flex flex-col gap-2 text-sm font-light text-gray-700'>
                 <p className='flex gap-2'>
-<<<<<<< HEAD
                     <input type="checkbox" onChange={toggleCategory} name="" id="" value={'Men'} className='w-3'/>Men
                 </p>
                 <p className='flex gap-2'>
@@ -98,15 +76,6 @@ const Collection = () => {
                 </p>
                 <p className='flex gap-2'>
                     <input type="checkbox" onChange={toggleCategory} name="" id="" value={'Kids'} className='w-3'/>Kids
-=======
-                    <input type="checkbox" onChange={toggleCategary} name="" id="" value={'Men'} className='w-3'/>Men
-                </p>
-                <p className='flex gap-2'>
-                    <input type="checkbox" onChange={toggleCategary} name="" id="" value={'Women'} className='w-3'/>Women
-                </p>
-                <p className='flex gap-2'>
-                    <input type="checkbox" onChange={toggleCategary} name="" id="" value={'kids'} className='w-3'/>kids
->>>>>>> 555b1b9c59e4828476de91d078265de6d9a94efc
                 </p>
 
               </div>
@@ -117,7 +86,6 @@ const Collection = () => {
             <p className='mb-3 text-sm font-medium'>TYPE</p>
               <div className='flex flex-col gap-2 text-sm font-light text-gray-700'>
                 <p className='flex gap-2'>
-<<<<<<< HEAD
                     <input type="checkbox" onChange={toggleSubCategory} name="" id="" value={'T-shirts'} className='w-3'/>T-shirts
                 </p>
                 <p className='flex gap-2'>
@@ -131,15 +99,6 @@ const Collection = () => {
                 </p>
                 <p className='flex gap-2'>
                     <input type="checkbox" onChange={toggleSubCategory} name="" id="" value={'Sweaters'} className='w-3'/>Sweaters
-=======
-                    <input type="checkbox" onChange={toggleSubCategary} name="" id="" value={'Topwear'} className='w-3'/>Topwear
-                </p>
-                <p className='flex gap-2'>
-                    <input type="checkbox" onChange={toggleSubCategary} name="" id="" value={'Bottomwear'} className='w-3'/>Bottomwear
-                </p>
-                <p className='flex gap-2'>
-                    <input type="checkbox" onChange={toggleSubCategary} name="" id="" value={'Winterwear'} className='w-3'/>Winterwear
->>>>>>> 555b1b9c59e4828476de91d078265de6d9a94efc
                 </p>
 
               </div>
