70b345ee390509bc9fde8f0212d16ad63eefd4b6
Darshan801 <darshankunwar79@gmail.com>
2025-03-18 10:04:00 +0545

    product.jsx

--- src/pages/Collection.jsx
+++ src/pages/Collection.jsx
@@ -5,7 +5,7 @@ import Title from '../components/Title';
 import ProductItem from '../components/ProductItem';
 
 const Collection = () => {
-  const { products }=useContext(ShopContext);
+  const { products , search , showSearch }=useContext(ShopContext);
   const [showfilter,setshowfilter]=useState(false);
   const[FilterProducts,setFilterProducts]=useState([]);
   const [category,setCategory]=useState([]);
@@ -36,6 +36,11 @@ const Collection = () => {
   
   const applyFilter=()=>{
     let productCopy=products.slice();
+    if (showSearch && search) {
+      productCopy=productCopy.filter(item=>item.name.toLowerCase().includes(search.toLowerCase()))
+    }
+
+
     if (category.length>0) {
       productCopy=productCopy.filter(item=>category.includes(item.category));
     }
@@ -69,7 +74,7 @@ const Collection = () => {
 
   useEffect(()=>{
     applyFilter();
-  },[category,subcategory])
+  },[category,subcategory,search,showSearch])
 
   useEffect(()=>{
     sortProduct();
